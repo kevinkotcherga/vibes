cb04d4da852b91ef1120bb255e662dc2de40bd04
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2021-12-01 16:18:29 +0100

    Link to home

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,12 @@ class User < ApplicationRecord
   has_many :responses, through: :question_users
   has_one :profile, dependent: :destroy
   has_many :messages, dependent: :destroy
-  has_many :matchings, foreign_key: "from_user_id", dependent: :destroy
-  has_many :chatrooms, through: :matchings
+
+  has_many :created_matchings, foreign_key: "from_user_id", class_name: 'Matching', dependent: :destroy
+  has_many :received_matchings, foreign_key: "to_user_id",  class_name: 'Matching', dependent: :destroy
+
+  has_many :chatrooms, through: :created_matchings
+  has_many :chatrooms, through: :received_matchings
 
   has_one :profile, dependent: :destroy
 
