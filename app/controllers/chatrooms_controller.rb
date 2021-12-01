92a1f5a704a6ae11b4e86cb97659393cc86f5dd4
jules chevrant breton <jules.chevrant@hotmail.fr>
2021-12-01 12:02:18 +0100

    Changement du titre de la conversation

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,5 +2,7 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @matching = @chatroom.matching
+    @user = User.find(@matching.to_user_id)
   end
 end
