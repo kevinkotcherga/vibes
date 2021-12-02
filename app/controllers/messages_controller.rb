fdf2d1cb2ce65295cac58eee174794970d4b2668
jules chevrant breton <jules.chevrant@hotmail.fr>
2021-12-02 11:27:24 +0100

    Modif back chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,10 +5,10 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
-      # redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
       ChatroomChannel.broadcast_to(
         @chatroom,
-        render_to_string(partial: "message", locals: {message: @message, first_user: @chatroom.messages.first.user.id})
+        render_to_string(partial: "message", locals: { message: @message, first_user: @chatroom.messages.first.user.id })
       )
     else
       render "chatrooms/show"
@@ -19,3 +19,6 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:content)
   end
 end
+
+
+# anchor: "message-#{message.id}"
